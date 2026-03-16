922e0718fbc58d85e4160f91c5a9d049bb448ce5
TYrueouJeL <guerinremi.pro@gmail.com>
2026-03-16 18:21:22 +0100

    Add customer modification functionality and form

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Form\CustomerFormType;
 use App\Repository\RentalOrderRepository;
 use App\Repository\UserVehicleRepository;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -59,6 +62,31 @@ final class CustomerController extends AbstractController
             'userVehicles' => $userVehicles,
             'rentalVehicles' => $rentalVehicles,
             'upcomingRentalVehicles' => $upcomingRentalVehicles,
+        ]);
+    }
+
+    #[Route('/customer/modify', name: 'app_customer_modify')]
+    public function modify(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(CustomerFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vos informations ont été mises à jour avec succès.');
+
+            return $this->redirectToRoute('app_customer');
+        }
+
+        return $this->render('customer/modify.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
